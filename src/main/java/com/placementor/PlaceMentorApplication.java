43e3a099442ed70af5b598e8331000a285114a96
kavya <kavya@lenovo.dlink>
2026-01-04 21:56:35 +0530

    Initial Spring Boot Project setup

---
+++ src/main/java/com/placementor/PlaceMentorApplication.java
@@ -0,0 +1,13 @@
+package com.placementor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceMentorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceMentorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/placementor/PlaceMentorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.placementor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceMentorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
